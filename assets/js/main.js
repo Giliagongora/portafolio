816bc0d69bc18293169cb45e2c15920a58155cb3
Gilia Gongora <giliagongora.g@gmail.com>
2017-08-23 23:12:42 -0300

    Verificando detalles

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+
+// funcionalidad al navbar
+$(".button-collapse").sideNav();
+
+
+//slider
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
+
+});
