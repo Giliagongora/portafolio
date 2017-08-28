5313eec1c2c9ce76d07c01356110959595ede76e
Gilia Gongora <giliagongora.g@gmail.com>
2017-08-27 23:49:33 -0300

    navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,19 @@
 $(document).ready(function(){
 
 // funcionalidad al navbar
-$(".button-collapse").sideNav();
-
+  $('.button-collapse').sideNav({
+      menuWidth: 170, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens, 
+    }
+  );
 
 //slider
     $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
 
-//footer
 
 
 
